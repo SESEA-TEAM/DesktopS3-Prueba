da90e904c2378fb3b1ce65f12f402f2e6dea711e
Haisahar <197O00983@itsx.edu.mx>
2022-10-13 16:55:39 -0500

    Modificación den Crear Usuario
    xd

--- Sistema de sanciones/Crear_Usuario.Designer.cs
+++ Sistema de sanciones/Crear_Usuario.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new Sistema_de_sanciones.Templates.TextBox();
+            this.textBox2 = new Sistema_de_sanciones.Templates.TextBox();
+            this.textBox3 = new Sistema_de_sanciones.Templates.TextBox();
+            this.textBox4 = new Sistema_de_sanciones.Templates.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,12 +44,76 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Crear Usuarios";
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox1.BorderSize = 2;
+            this.textBox1.Location = new System.Drawing.Point(121, 91);
+            this.textBox1.Multiline = false;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox1.PasswordChar = false;
+            this.textBox1.Size = new System.Drawing.Size(250, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Texts = "";
+            this.textBox1.UnderlinedStyle = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox2.BorderSize = 2;
+            this.textBox2.Location = new System.Drawing.Point(178, 188);
+            this.textBox2.Multiline = false;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox2.PasswordChar = false;
+            this.textBox2.Size = new System.Drawing.Size(250, 30);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Texts = "";
+            this.textBox2.UnderlinedStyle = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox3.BorderSize = 2;
+            this.textBox3.Location = new System.Drawing.Point(318, 319);
+            this.textBox3.Multiline = false;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox3.PasswordChar = false;
+            this.textBox3.Size = new System.Drawing.Size(250, 30);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Texts = "";
+            this.textBox3.UnderlinedStyle = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox4.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.textBox4.BorderSize = 2;
+            this.textBox4.Location = new System.Drawing.Point(205, 22);
+            this.textBox4.Multiline = false;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Padding = new System.Windows.Forms.Padding(7);
+            this.textBox4.PasswordChar = false;
+            this.textBox4.Size = new System.Drawing.Size(250, 30);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Texts = "";
+            this.textBox4.UnderlinedStyle = false;
+            // 
             // Crear_Usuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Crear_Usuario";
@@ -58,5 +126,9 @@
         #endregion
 
         private Label label1;
+        private Templates.TextBox textBox1;
+        private Templates.TextBox textBox2;
+        private Templates.TextBox textBox3;
+        private Templates.TextBox textBox4;
     }
 }
