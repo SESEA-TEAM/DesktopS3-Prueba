38fd0c88ee33407604e34ef2a5c9e2df68d290fd
EdgarCrSt <73854445+EdgarCrSt@users.noreply.github.com>
2022-10-16 20:38:26 -0500

    CapturaPS
    Agregue la ventana de captura de particular sancionado

---
+++ Sistema de sanciones/CapturaPS.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_sanciones
+{
+    public partial class CapturaPS : Form
+    {
+        public CapturaPS()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rjComboBox5_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox43_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
