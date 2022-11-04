7c5fad2302a4a06e04c9196ed128687179b6651f
EdgarCrSt <73854445+EdgarCrSt@users.noreply.github.com>
2022-11-04 10:13:32 -0600

    Update Conexion
    se agregan las funciones para conectarse a las otras 2 bases de datos SPS (servidores publicos sancionados) y PS (particulares sancionados)

--- Sistema de sanciones/ConexionBD/Conexion.cs
+++ Sistema de sanciones/ConexionBD/Conexion.cs
@@ -11,6 +11,8 @@ namespace Sistema_de_sanciones.ConexionBD
     public class Conexion
     {
         private SqlConnection ConexionBD = new SqlConnection("Server=localhost;DataBase=Usuario; Integrated Security= true;");
+        private SqlConnection ConexionBDSPS = new SqlConnection("Server=localhost;DataBase=PublicosSancionados; Integrated Security= true;");
+        private SqlConnection ConexionBDPS = new SqlConnection("Server=localhost;DataBase=ParticularesSancionados; Integrated Security= true;");
 
         public SqlConnection AbrirConexion()
         {
@@ -26,6 +28,40 @@ namespace Sistema_de_sanciones.ConexionBD
             if (ConexionBD.State == ConnectionState.Open)
                 ConexionBD.Close();
             return ConexionBD;
+
+        }
+
+        public SqlConnection AbrirConexionSPS()
+        {
+            if (ConexionBDSPS.State == ConnectionState.Closed)
+                ConexionBDSPS.Open();
+            return ConexionBDSPS;
+
+
+        }
+
+        public SqlConnection CerrarConexionSPS()
+        {
+            if (ConexionBDSPS.State == ConnectionState.Open)
+                ConexionBDSPS.Close();
+            return ConexionBDSPS;
+
+        }
+
+        public SqlConnection AbrirConexionPS()
+        {
+            if (ConexionBDPS.State == ConnectionState.Closed)
+                ConexionBDPS.Open();
+            return ConexionBDPS;
+
+
+        }
+
+        public SqlConnection CerrarConexionPS()
+        {
+            if (ConexionBDPS.State == ConnectionState.Open)
+                ConexionBDPS.Close();
+            return ConexionBDPS;
 
         }
 
