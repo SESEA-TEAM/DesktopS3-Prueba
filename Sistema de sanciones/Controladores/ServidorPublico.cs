1dfd1730ac8a6709eb9a390789367fac3186dafd
Arklone07 <112571551+Arklone07@users.noreply.github.com>
2022-11-22 17:44:53 -0600

    Controlador servidor publico
    Es para la validación de los datos en captura, es que se me olvido subirlo con el otro xd

---
+++ Sistema de sanciones/Controladores/ServidorPublico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_sanciones.Controladores
+{
+    public class ServidorPublico
+    {
+        public Metodo_validacion textBoxEvent = new Metodo_validacion();
+    }
+}
