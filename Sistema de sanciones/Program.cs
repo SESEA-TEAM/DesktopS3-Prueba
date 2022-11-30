429d02e82785a6700ae0dc783cd3a66eea057e02
Omar <70543870+RompeBucles@users.noreply.github.com>
2022-11-30 13:05:10 -0600

    Update

--- Sistema de sanciones/Program.cs
+++ Sistema de sanciones/Program.cs
@@ -11,7 +11,8 @@ namespace Sistema_de_sanciones
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(new Listar_Proveedores());
+         
         }
     }
 }
